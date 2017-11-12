9a6465c0c0aa83ee58d561b4ecbc1332efdfc2fd
Jonas <N/A>
2017-11-12 15:14:24 +0100

    Testing the GetVideos Method

--- VideoOnDemand.UI/Controllers/HomeController.cs
+++ VideoOnDemand.UI/Controllers/HomeController.cs
@@ -22,6 +22,8 @@ namespace VideoOnDemand.UI.Controllers
             var courses = rep.GetCourses("4ad684f8-bb70-4968-85f8-458aa7dc19a3");
             var course = rep.GetCourse("4ad684f8-bb70-4968-85f8-458aa7dc19a3", 1);
             var video = rep.GetVideo("4ad684f8-bb70-4968-85f8-458aa7dc19a3", 1);
+            var videos = rep.GetVideos("4ad684f8-bb70-4968-85f8-458aa7dc19a3");
+            var videosForModule = rep.GetVideos("4ad684f8-bb70-4968-85f8-458aa7dc19a3", 1);
 
             if (!_signInManager.IsSignedIn(User))
                 return RedirectToAction("Login", "Account");
