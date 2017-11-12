04d94f470cf49c17befa60778e13920b2862d8bb
Jonas <N/A>
2017-11-12 11:33:50 +0100

    Redirecting to the Login View

--- VideoOnDemand.UI/Controllers/HomeController.cs
+++ VideoOnDemand.UI/Controllers/HomeController.cs
@@ -1,17 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using VideoOnDemand.UI.Models;
+using Microsoft.AspNetCore.Identity;
+using VideoOnDemand.Data.Data.Entities;
+
 
 namespace VideoOnDemand.UI.Controllers
 {
     public class HomeController : Controller
     {
+        private SignInManager<User> _signInManager;
+
+        public HomeController(SignInManager<User> signInMgr)
+        {
+            _signInManager = signInMgr;
+        }
+
         public IActionResult Index()
         {
+            if (!_signInManager.IsSignedIn(User))
+                return RedirectToAction("Login", "Account");
+
             return View();
         }
 
