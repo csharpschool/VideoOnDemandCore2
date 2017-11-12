375357d70bcef6bf35fb38333fe4cc6d010a94fc
Jonas <N/A>
2017-11-12 15:15:48 +0100

    Deleted all the test variables and the rep instance from the Index action

--- VideoOnDemand.UI/Controllers/HomeController.cs
+++ VideoOnDemand.UI/Controllers/HomeController.cs
@@ -18,13 +18,6 @@ namespace VideoOnDemand.UI.Controllers
 
         public IActionResult Index()
         {
-            var rep = new MockReadRepository();
-            var courses = rep.GetCourses("4ad684f8-bb70-4968-85f8-458aa7dc19a3");
-            var course = rep.GetCourse("4ad684f8-bb70-4968-85f8-458aa7dc19a3", 1);
-            var video = rep.GetVideo("4ad684f8-bb70-4968-85f8-458aa7dc19a3", 1);
-            var videos = rep.GetVideos("4ad684f8-bb70-4968-85f8-458aa7dc19a3");
-            var videosForModule = rep.GetVideos("4ad684f8-bb70-4968-85f8-458aa7dc19a3", 1);
-
             if (!_signInManager.IsSignedIn(User))
                 return RedirectToAction("Login", "Account");
 
