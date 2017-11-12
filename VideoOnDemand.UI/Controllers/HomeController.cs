904e4844dc6cfac7b94137b93378f71348dcd144
Jonas <N/A>
2017-11-12 15:11:26 +0100

    Testing the GetVideo Method

--- VideoOnDemand.UI/Controllers/HomeController.cs
+++ VideoOnDemand.UI/Controllers/HomeController.cs
@@ -21,6 +21,7 @@ namespace VideoOnDemand.UI.Controllers
             var rep = new MockReadRepository();
             var courses = rep.GetCourses("4ad684f8-bb70-4968-85f8-458aa7dc19a3");
             var course = rep.GetCourse("4ad684f8-bb70-4968-85f8-458aa7dc19a3", 1);
+            var video = rep.GetVideo("4ad684f8-bb70-4968-85f8-458aa7dc19a3", 1);
 
             if (!_signInManager.IsSignedIn(User))
                 return RedirectToAction("Login", "Account");
