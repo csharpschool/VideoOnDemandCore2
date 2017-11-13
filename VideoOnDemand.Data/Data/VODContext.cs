30f92a1e359079c48c6978dcfc4856a4e23ad3be
Jonas <N/A>
2017-11-13 09:53:35 +0100

    Adding the Entity Classes to the VODContext

--- VideoOnDemand.Data/Data/VODContext.cs
+++ VideoOnDemand.Data/Data/VODContext.cs
@@ -1,11 +1,19 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 using VideoOnDemand.Data.Data.Entities;
 
 namespace VideoOnDemand.Data.Data
 {
     public class VODContext : IdentityDbContext<User>
     {
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Download> Downloads { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<Module> Modules { get; set; }
+        public DbSet<UserCourse> UserCourses { get; set; }
+        public DbSet<Video> Videos { get; set; }
+
         public VODContext(DbContextOptions<VODContext> options) : base(options)
         {
         }
@@ -13,7 +21,15 @@ namespace VideoOnDemand.Data.Data
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
-        }
 
+            // Composite key
+            builder.Entity<UserCourse>().HasKey(uc => new { uc.UserId, uc.CourseId });
+
+            // Restrict cascading deletes
+            foreach (var relationship in builder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+        }
     }
 }
