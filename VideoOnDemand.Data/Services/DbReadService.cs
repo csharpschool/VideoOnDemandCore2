7410e4c08d5c25c77141c9491d077ad28967b3e0
Jonas <N/A>
2017-11-13 10:23:14 +0100

    Adding the Service Interface and Class

---
+++ VideoOnDemand.Data/Services/DbReadService.cs
@@ -0,0 +1,14 @@
+﻿using VideoOnDemand.Data.Data;
+
+namespace VideoOnDemand.Data.Services
+{
+    public class DbReadService : IDbReadService
+    {
+        private VODContext _db;
+        public DbReadService(VODContext db)
+        {
+            _db = db;
+        }
+
+    }
+}
---
+++ VideoOnDemand.Data/Services/IDbReadService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VideoOnDemand.Data.Services
+{
+    public interface IDbReadService
+    {
+    }
+}
