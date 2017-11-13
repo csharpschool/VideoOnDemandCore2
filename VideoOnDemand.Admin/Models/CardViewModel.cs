53dd3230cfac786cce8dd69fd924089626717932
Jonas <N/A>
2017-11-13 13:14:23 +0100

    Adding the CardViewModel Class

---
+++ VideoOnDemand.Admin/Models/CardViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VideoOnDemand.Admin.Models
+{
+    public class CardViewModel
+    {
+        public int Count { get; set; }
+        public string Description { get; set; }
+        public string Icon { get; set; }
+        public string Url { get; set; }
+        public string BackgroundColor { get; set; }
+    }
+}
