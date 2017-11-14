fe1df01f3d45328358d819cf32250a1b478e3a4a
Jonas <N/A>
2017-11-14 10:12:09 +0100

    Adding the UserPageModel Class

---
+++ VideoOnDemand.Admin/Models/UserPageModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VideoOnDemand.Admin.Models
+{
+    public class UserPageModel
+    {
+        [Required]
+        [Display(Name = "User Id")]
+        public string Id { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [Display(Name = "Is Admin")]
+        public bool IsAdmin { get; set; }
+    }
+}
