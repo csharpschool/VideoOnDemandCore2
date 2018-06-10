069ce04e959d87ac5a483d27754ae7d091f2fd37
Jonas <N/A>
2018-06-10 16:33:52 +0200

    Adding the free VideoJS video player.

---
+++ VideoOnDemand.UI/wwwroot/js/videoJS.js
@@ -0,0 +1,10 @@
+﻿function videoJS(video) {
+    var container = document.getElementById("video");
+
+    videojs(container, {
+        controls: true,
+        techOrder: ["youtube"],
+        sources: [{ type: "video/youtube", src: video }]
+    }, function () {
+    });
+}
